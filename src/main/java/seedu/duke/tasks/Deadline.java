3b8e9dfbfd8576c7d3fa86a0d5eccb1945e44382
Steven Wijaya <stevenwjy149@gmail.com>
2019-08-28 16:57:00 +0800

    Update chatbot to recognise dates and times.

--- src/main/java/seedu/duke/tasks/Deadline.java
+++ src/main/java/seedu/duke/tasks/Deadline.java
@@ -1,11 +1,28 @@
 package seedu.duke.tasks;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
 public class Deadline extends Task {
     private String timeDescription;
 
     public Deadline(String description, String timeDescription) {
         super(TaskType.DEADLINE, description);
-        this.timeDescription = timeDescription;
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        DateTimeFormatter targetFormatter = DateTimeFormatter
+                .ofLocalizedDateTime(FormatStyle.LONG, FormatStyle.SHORT);
+
+        if (timeDescription.matches("[0-9]{2}/[0-9]{2}/[0-9]{4} [0-9]{2}:[0-9]{2}")) {
+            try {
+                this.timeDescription = LocalDateTime.parse(timeDescription, formatter).format(targetFormatter);
+            } catch (Exception e) {
+                this.timeDescription = timeDescription;
+            }
+        } else {
+            this.timeDescription = timeDescription;
+        }
     }
 
     @Override
