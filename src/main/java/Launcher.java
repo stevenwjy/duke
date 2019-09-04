766e46b1e2b6b6d11963313d58b2354daf43b014
Steven Wijaya <stevenwjy149@gmail.com>
2019-09-04 15:01:52 +0800

    Add initial template (layout) for Duke chat app

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,8 @@
+import javafx.application.Application;
+import seedu.duke.Duke;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Duke.class, args);
+    }
+}
--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,17 +1,37 @@
 package seedu.duke;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import seedu.duke.commands.Command;
 import seedu.duke.commands.ExitCommand;
 import seedu.duke.io.InputReader;
 import seedu.duke.io.OutputWriter;
 import seedu.duke.tasks.TaskManager;
 
-public class Duke {
+public class Duke extends Application {
     private InputReader inputReader = new InputReader();
     private OutputWriter outputWriter = new OutputWriter();
     private TaskManager taskManager;
 
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
     public Duke(String filePath) throws Exception {
+        this.taskManager = new TaskManager(filePath);
+    }
+
+    public Duke() throws Exception {
+        String filePath = "data/tasks.txt";
         this.taskManager = new TaskManager(filePath);
     }
 
@@ -56,5 +76,54 @@ public class Duke {
         } catch (Exception e) {
             System.out.println("Failed to run the main program, error: " + e.getMessage());
         }
+    }
+
+    @Override
+    public void start(Stage stage) {
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+        formatWindow(stage, mainLayout);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void formatWindow(Stage stage, AnchorPane mainLayout) {
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput, 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
     }
 }
